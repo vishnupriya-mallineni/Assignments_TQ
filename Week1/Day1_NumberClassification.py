ed6632a1f7e8ef6fdf89d5bffeee31bb5edd13f2
vishnupriya-mallineni <113497055+vishnupriya-mallineni@users.noreply.github.com>
2026-09-10 18:03:39 -0400

    Update Day1_NumberClassification.py

--- Week1/Day1_NumberClassification.py
+++ Week1/Day1_NumberClassification.py
@@ -6,16 +6,4 @@ elif number < 0:
 else:
     print("The number is Zero")
 
-print("\n ====EVEN / ODD CHECKER=== ")
 
-def check_even_odd(number):
-    if number % 2 == 0:
-        return "Even"
-    else:
-        return "Odd"
-print("Enter 5 numbers:")
-
-for i in range(5):
-    number = int(input("Enter number: "))
-    result = check_even_odd(number)
-    print(number, "is", result)
