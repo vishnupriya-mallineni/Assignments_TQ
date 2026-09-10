bc35a2e6c5f62f3f11e811720a1a27eaaefc13ea
vishnupriya-mallineni <113497055+vishnupriya-mallineni@users.noreply.github.com>
2026-09-10 18:04:38 -0400

    Create Day2_ListOperations.py

---
+++ Week1/Day2_ListOperations.py
@@ -0,0 +1,30 @@
+numbers = [30, 10, 50, 20, 40]
+print("Original List:")
+print(numbers)
+
+
+# Append
+numbers.append(60)
+print("\nAfter append(60):")
+print(numbers)
+
+
+# Insert
+numbers.insert(2, 25)
+print("\nAfter insert(2, 25):")
+print(numbers)
+
+# Remove
+numbers.remove(10)
+print("\nAfter remove(10):")
+print(numbers)
+
+# Sort
+numbers.sort()
+print("\nAfter sort():")
+print(numbers)
+
+# Reverse
+numbers.reverse()
+print("\nAfter reverse():")
+print(numbers)
