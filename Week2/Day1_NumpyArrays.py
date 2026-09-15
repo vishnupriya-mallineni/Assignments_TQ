97f29831deab951ca61ac63fc70e29db578f0599
vishnupriya-mallineni <113497055+vishnupriya-mallineni@users.noreply.github.com>
2026-09-15 09:00:29 -0400

    Create Day1_NumpyArrays.py

---
+++ Week2/Day1_NumpyArrays.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+array_1d = np.array([10, 20, 30, 40, 50])
+
+print("1D Array:")
+print(array_1d)
+print("Shape:", array_1d.shape)
+print("Size:", array_1d.size)
+print("Dimensions:", array_1d.ndim)
+
+array_2d = np.array([
+    [10, 20, 30],
+    [40, 50, 60]
+])
+
+print("\n2D Array:")
+print(array_2d)
+print("Shape:", array_2d.shape)
+print("Size:", array_2d.size)
+print("Dimensions:", array_2d.ndim)
